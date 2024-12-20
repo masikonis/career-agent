f70b2b7e6f3bafc5188f27ab5796503f71a46663
Nerijus Masikonis <nerijus@masikonis.lt>
2024-12-20 06:07:58 +0200

    Update deploy_job_ads_scraping.py

--- src/workflows/deployments/deploy_job_ads_scraping.py
+++ src/workflows/deployments/deploy_job_ads_scraping.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
 from prefect import flow
+from prefect.deployments import DeploymentImage
 
 from src.workflows.job_ads_scraping import job_ads_scraping_flow
 
@@ -8,11 +9,8 @@ if __name__ == "__main__":
     deployment = job_ads_scraping_flow.to_deployment(
         name="job-ads-scraping",
         work_pool_name="career-crew-pool",
-        interval=timedelta(hours=24),
-        storage={
-            "type": "github",
-            "repository": "masikonis/career-crew",
-            "reference": "main",
-        },
+        interval=timedelta(hours=4),
+        path="./",
+        entrypoint="src/workflows/job_ads_scraping.py:job_ads_scraping_flow",
     )
     deployment.apply()
