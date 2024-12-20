506e97c4c9d8a4249cec7448ed3295be95fd05cb
Nerijus Masikonis <nerijus@masikonis.lt>
2024-12-20 06:11:54 +0200

    Update deploy_job_ads_scraping.py

--- src/workflows/deployments/deploy_job_ads_scraping.py
+++ src/workflows/deployments/deploy_job_ads_scraping.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     job_ads_scraping_flow.from_source(
         source="https://github.com/masikonis/career-crew.git",
         entrypoint="src/workflows/job_ads_scraping.py:job_ads_scraping_flow",
+        reference="main",
     ).deploy(
         name="job-ads-scraping",
         work_pool_name="career-crew-pool",
         interval=timedelta(hours=4),
+        build=False,
     )
